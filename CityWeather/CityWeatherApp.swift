7b608d18aabb40528abc8662298158a83a58e948
Michał Krupa <michal.krupa@proexe.pl>
2024-01-31 10:19:47 +0100

    initial commit

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by Michał Krupa on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
